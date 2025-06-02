c753bd85a4d36a04b8eb2cab46612c749668c6b5
AbhayGhante <76439611+AbhayGhante@users.noreply.github.com>
2025-06-02 17:14:50 +0530

    add2

--- script/start_container.sh
+++ script/start_container.sh
@@ -8,4 +8,4 @@ echo "Abhay@2001" | docker login docker.io --username abhayghante --password-std
 #pull
 docker pull abhayghante/simple-python-app:v1
 #run
-docker run -d -p 5000:5000 abhayghante/simple-python-app:v1
+docker run -d -p 3000:3000 abhayghante/simple-python-app:v1
